bece3d7950e3a29b61351c6e284783ac9dfcae58
Roman <Roman.vladimirovich23@gmail.com>
2023-08-14 17:41:11 +0500

    Add files via upload

---
+++ Settings.py
@@ -0,0 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+valid_email = os.getenv('valid_email')
+valid_password = os.getenv('valid_password')
+unvalid_email = os.getenv('qwqfwffew@mail.ru')
+unvalid_password = os.getenv('54321')
+unvalid_auth_key = os.getenv('ea738148a1f19838e1c5d1413877f3691a3731380e733e877b0ae729')
+
